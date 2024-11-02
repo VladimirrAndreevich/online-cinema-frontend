04afb38b94741c0317ee425a54ca98b9553b7d0e
Vladimir <ryzhovvladimirr@gmail.com>
2024-11-02 20:59:59 +0100

    feat: add video player

--- app/components/screens/SingleMovie/SingleMovie.tsx
+++ app/components/screens/SingleMovie/SingleMovie.tsx
@@ -1,12 +1,21 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { IMoviePage } from "../../../../pages/movie/[slug]";
 import Meta from "@/utils/meta/Meta";
 import Banner from "@/components/ui/banner/Banner";
 import SubHeading from "@/components/ui/heading/SubHeading";
 import Gallery from "@/components/ui/gallery/Gallery";
 import Content from "./Content/Content";
+import dynamic from "next/dynamic";
+
+const DynamicPlayer = dynamic(() => import("@/ui/VideoPlayer/VideoPlayer"), {
+	ssr: false,
+});
 
 const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
+	useEffect(() => {
+		console.log(movie.videoUrl);
+	}, []);
+
 	return (
 		<Meta title={movie.title || ""} description={`Watch ${movie.title}`}>
 			<Banner
@@ -14,7 +23,7 @@ const SingleMovie: FC<IMoviePage> = ({ movie, similarMovies }) => {
 				Detail={() => <Content movie={movie} />}
 			/>
 
-			{/* Video player */}
+			<DynamicPlayer videoSource={movie.videoUrl} slug={movie.slug} />
 
 			<div className="mt-12">
 				<SubHeading title="Similar movies" />
